9bdbf331fb61b719c0e84f1d2cfb3945dedc7ebc
MaximeRoberti <maxime.roberti@gmail.com>
2018-03-26 16:26:31 +0200

    Created seeds for category and recipe

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Recipe.destroy_all
+Category.destroy_all
+
+puts 'Creating categories...'
+asian = Category.create!(name: 'asian')
+vegetarian = Category.create!(name: 'vegetarian')
+
+puts 'Creating recipes...'
+Recipe.create!(name: 'Donburi', description: "Fresh donburi made with love", prep_time: 10, cooking_time: 20, category: asian)
+Recipe.create!(name: 'Vegetarian lasagna', description: "Healthy lasagna made with natural ingredients", prep_time: 15, cooking_time: 30, category: vegetarian)
+puts 'Finished!'
