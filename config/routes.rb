9bba266ef49a71b373634e6d792bcbdb0c07cd87
Diego Gilon <diego.gilon@gmail.com>
2018-03-26 15:03:59 +0200

    removing extra routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
-  get 'recipes/index'
-
-  get 'recipes/show'
-
-  get 'categories/index'
-
-  get 'categories/show'
-
   devise_for :users
-  root to: 'pages#home'
+  root to: 'category#index'
 
   resources :category, only: [:index]
   resources :recipe, only: [:index, :show]
