7e039378a5274d67e8bb326779c6047bf91e1afa
Diego Gilon <diego.gilon@gmail.com>
2018-03-27 12:31:31 +0200

    filter by category

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,13 @@
 class RecipesController < ApplicationController
+  skip_before_action :authenticate_user!
   def index
+    if params[:category]
+      @category = Category.find_by(name: params[:category])
+      @recipes = Recipe.where(category: @category)
+    else
+      @recipes = Recipe.all
+    end
+
   end
 
   def show
