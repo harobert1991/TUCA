1f0e99ba4302f85a2458754724bfdab6dcbf7172
Hadrien Robert <robert.hadrien@gmail.com>
2018-03-28 15:16:31 +0200

    Dose Done

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,3 +1,6 @@
 class Step < ApplicationRecord
   belongs_to :recipe, dependent: :destroy
+  has_many :doses, dependent: :destroy
+
+  accepts_nested_attributes_for :doses
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@ Recipe.destroy_all
 Category.destroy_all
 
 
+
 puts 'Creating categories...'
 asian = Category.create!(name: 'asian', photo: "http://res.cloudinary.com/dqsodpjur/image/upload/v1522147213/po2tdf0b727h1swcpas3.jpg")
 vegetarian = Category.create!(name: 'vegetarian', photo: "http://res.cloudinary.com/dqsodpjur/image/upload/v1522144043/r1egdekahnrrrhs7nezh.jpg")
@@ -19,10 +20,18 @@ donburi = Recipe.create!(name: 'Donburi', description: "Fresh donburi made with 
 veg_lasagna = Recipe.create!(name: 'Vegetarian lasagna', description: "Healthy lasagna made with natural ingredients", prep_time: 15, cooking_time: 30, category: vegetarian, photo: "http://res.cloudinary.com/dqsodpjur/image/upload/v1522144043/r1egdekahnrrrhs7nezh.jpg")
 
 puts 'Creating steps...'
-Step.create!(sequence: 1, photo: "http://res.cloudinary.com/dqsodpjur/image/upload/v1522147213/po2tdf0b727h1swcpas3.jpg", description: "Cook the potatoes", step_time: 15, recipe: donburi)
-Step.create!(sequence: 2, photo: "http://res.cloudinary.com/dqsodpjur/image/upload/v1522144043/r1egdekahnrrrhs7nezh.jpg", description: "Cut the onions", step_time: 2, recipe: donburi)
-Step.create!(sequence: 3, photo: "http://res.cloudinary.com/dqsodpjur/image/upload/v1522147213/po2tdf0b727h1swcpas3.jpg", description: "Cook the meatballs", step_time: 15, recipe: donburi)
-Step.create!(sequence: 4, photo: "http://res.cloudinary.com/dqsodpjur/image/upload/v1522144043/r1egdekahnrrrhs7nezh.jpg", description: "Cook the chicory", step_time: 5, recipe: donburi)
-Step.create!(sequence: 1, photo: "http://res.cloudinary.com/dqsodpjur/image/upload/v1522147213/po2tdf0b727h1swcpas3.jpg", description: "This is the first step of the veg_lasagna recipe", step_time: 10, recipe: veg_lasagna)
-Step.create!(sequence: 2, photo: "http://res.cloudinary.com/dqsodpjur/image/upload/v1522144043/r1egdekahnrrrhs7nezh.jpg", description: "This is the second step of the veg_lasagna recipe", step_time: 15, recipe: veg_lasagna)
+s1 =Step.create!(sequence: 1, photo: "http://res.cloudinary.com/dqsodpjur/image/upload/v1522147213/po2tdf0b727h1swcpas3.jpg", description: "Cook the potatoes", step_time: 15, recipe: donburi)
+s2 =Step.create!(sequence: 2, photo: "http://res.cloudinary.com/dqsodpjur/image/upload/v1522144043/r1egdekahnrrrhs7nezh.jpg", description: "Cut the onions", step_time: 2, recipe: donburi)
+s3 =Step.create!(sequence: 3, photo: "http://res.cloudinary.com/dqsodpjur/image/upload/v1522147213/po2tdf0b727h1swcpas3.jpg", description: "Cook the meatballs", step_time: 15, recipe: donburi)
+s4 =Step.create!(sequence: 4, photo: "http://res.cloudinary.com/dqsodpjur/image/upload/v1522144043/r1egdekahnrrrhs7nezh.jpg", description: "Step 4", step_time: 5, recipe: donburi)
+s5 =Step.create!(sequence: 5, photo: "http://res.cloudinary.com/dqsodpjur/image/upload/v1522147213/po2tdf0b727h1swcpas3.jpg", description: "Step 5", step_time: 10, recipe: veg_lasagna)
+s6 =Step.create!(sequence: 6, photo: "http://res.cloudinary.com/dqsodpjur/image/upload/v1522144043/r1egdekahnrrrhs7nezh.jpg", description: "Step 6", step_time: 15, recipe: veg_lasagna)
+
+puts 'Creating doses...'
+
+d1 = Dose.create!(ingredient: "potato", quantity: 200, unit:"g", step: s1 )
+d2 = Dose.create!(ingredient: "szechuan sauce", quantity: 1, unit:"liter", step: s1 )
+d3 = Dose.create!(ingredient: "love", quantity: 1000, unit:"hearts", step: s1 )
+
 puts 'Finished!'
+
